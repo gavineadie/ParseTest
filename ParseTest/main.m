1bc5d0c6b49af34f12094222ea0c4cbad1f1b829
Gavin Eadie <gavin@507-AirBook.local>
2011-11-24 13:04:06 -0500

    Initial Commit

---
+++ ParseTest/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ParseTest
+//
+//  Created by Gavin Eadie on 11/24/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
